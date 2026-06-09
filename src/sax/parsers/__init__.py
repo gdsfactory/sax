7308958e7530390517dae903435e02bd1bf7f31d
Floris Laporte <floris.laporte@gmail.com>
2026-06-09 14:52:35 +0200

    Support mosaic (#119)
    * mosaic parser
    
    * update
    
    * expose

--- src/sax/parsers/__init__.py
+++ src/sax/parsers/__init__.py
@@ -2,12 +2,14 @@
 
 from .kfnetlist import parse_kfnetlist, parse_kfnetlist_recursive
 from .lumerical import parse_lumerical_dat, write_lumerical_dat
+from .mosaic import parse_mosaic
 from .touchstone import parse_touchstone, write_touchstone
 
 __all__ = [
     "parse_kfnetlist",
     "parse_kfnetlist_recursive",
     "parse_lumerical_dat",
+    "parse_mosaic",
     "parse_touchstone",
     "write_lumerical_dat",
     "write_touchstone",
